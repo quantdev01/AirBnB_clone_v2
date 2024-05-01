9d7b946b981600d68b8caa230a6cfcdd9be5b2e9
Daniel Kiyiki <danielngolu154@gmail.com>
2024-05-01 11:26:09 +0200

    Putted the original password

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,7 +2,8 @@
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- CREATE DATABASE IF NOT EXISTS performance_schema; 
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'Hbnb_dev_pwd@24';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+--CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'Hbnb_dev_pwd@24';
 USE hbnb_dev_db;
 -- GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES, RELOAD ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
 GRANT ALL PRIVILEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
