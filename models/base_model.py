b67b6fe6cc1fa3164dbe90711c6e07f8e94486f1
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-05-01 22:43:47 +0200

    Done with some task

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,11 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+import sqlalchemy
+
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class BaseModel:
@@ -10,10 +15,11 @@ class BaseModel:
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            from sqlalchemy import Column, String, Integer
+
+            self.id = Column(String(60), primary_key=True, nullable=False)
+            self.created_at = Column(datetime.utcnow(), nullable=False) 
+            self.updated_at = Column(datetime.utcnow(), nullable=False) 
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -31,6 +37,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -41,4 +48,13 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+
+        #Remove '_sa_intance_state' 
+        if '_sa_intance_state' in dictionary:
+            dictionary.pop('_sa_intance_state')
         return dictionary
+
+    def delete(self):
+        """call delete"""
+        # from models.engine.file_storage import FileStorage
+        del self
