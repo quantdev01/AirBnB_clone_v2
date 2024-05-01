9ba493aacb2711dc881b696263c7b491a755c6a5
Daniel Kiyiki <danielngolu154@gmail.com>
2024-05-01 11:42:25 +0200

    Db workd  now setup

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,8 +1,5 @@
 -- comment
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- CREATE DATABASE IF NOT EXISTS performance_schema;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'Hbnb_test_pwd@24';
-USE hbnb_test_db;
--- USE performance_schema; 
-GRANT ALL PRIVILEGES ON hbnb_test_db TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
