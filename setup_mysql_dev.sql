9fd53ddec7f3c831e5cafe49bd69a7410968865f
Daniel Kiyiki <danielngolu154@gmail.com>
2024-05-01 09:28:47 +0200

    Works now

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,8 @@
 -- db creation
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES, RELOAD ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
--- GRANT ALL PRIVILEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'Hbnb_dev_pwd@24';
+USE hbnb_dev_db;
+-- GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES, RELOAD ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
