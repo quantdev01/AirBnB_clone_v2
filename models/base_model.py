b65c88874eec36e6c5fe6ab168f917b9d1580c4d
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-05-05 03:45:08 +0200

    Moved storage.new() to save

--- models/base_model.py
+++ models/base_model.py
@@ -24,7 +24,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
 
             if kwargs:
                 for k, v in kwargs.items():
@@ -47,7 +46,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
-        # storage.new(self)
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
